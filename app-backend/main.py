05a73fbfdfde9c6a5870dbfbfb9a3845201ae553
Aleksandra <aleksandra.stepien@4soft.co>
2024-03-31 15:45:21 +0200

    feat: Introduce emotion prediction endpoint

--- app-backend/main.py
+++ app-backend/main.py
@@ -1,17 +1,40 @@
 import uvicorn
 import pickle
+import sys
+sys.path.append('utils')
 
 from fastapi import FastAPI
+from functions import clean_data, remove_stopwords_and_lemmatize
 
 app = FastAPI()
+
+def label_to_emotion(label: int) -> str:
+    label_to_emotion_dict = {
+		0 : "sadness",
+		1 : "joy",
+		2 : "love",
+		3 : "anger",
+		4 : "fear",
+		5 : "surprise"
+	}
+    return label_to_emotion_dict.get(label)
+     
 
 # Load the model from the pickle file
 with open('models/emotions_classifier_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
+# Load the tfidf vectorizer from the pickle file
+with open('models/tfidf.pkl', 'rb') as file:
+    tfidf_vectorizer = pickle.load(file)
+    
 @app.get("/predict")
-def model_prediction():
-	return {"Hello" : "World"}
+def model_prediction(sentence: str) -> str:
+	sentence = clean_data(sentence)
+	sentence = remove_stopwords_and_lemmatize(sentence)
+	sentence_tfidf = tfidf_vectorizer.transform([sentence])
+	prediction = model.predict(sentence_tfidf)
+	return label_to_emotion(prediction[0])
 
 
 if __name__ == "__main__":

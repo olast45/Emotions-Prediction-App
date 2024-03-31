46d914d7641b98c493219e5208a0ba18226fccd5
Aleksandra <aleksandra.stepien@4soft.co>
2024-03-31 22:51:30 +0200

    feat: Add a text field component

---
+++ app-frontend/src/components/textField.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+
+const TextFieldComponent = () => {
+    const [sentence, setSentence] = useState("");
+
+    return (
+        <div id="text-component">
+            <h2>Enter a sentence below</h2>
+            <TextField 
+                value={sentence}
+                onChange={(e) => {
+                    setSentence(e.target.value);
+                }}
+                className="TextField"
+            />
+            <h3>Your Entered Sentence is: {sentence} </h3>
+        </div>
+    );
+};
+
+export default TextFieldComponent;

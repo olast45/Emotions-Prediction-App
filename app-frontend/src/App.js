c7ffe77d2316f50576081e0373b384685e00d80b
Aleksandra <aleksandra.stepien@4soft.co>
2024-04-05 14:47:37 +0200

    refactor: Move fetch logic to App.js and pass props to prediction component

--- app-frontend/src/App.js
+++ app-frontend/src/App.js
@@ -1,13 +1,37 @@
 import "./css/App.css";
+import React, { useState, useEffect } from "react";
 import PredictionComponent from "./components/PredictionComponent";
 
-export default function App() {
-  return (
-    <>
-      <div id="title">
-        <h1>Emotions Prediction App</h1>
-      </div>
-      <PredictionComponent />
-    </>
+const App = () => {
+    const [sentence, setSentence] = useState("");
+    const [prediction, setPrediction] = useState("");
+
+    const sendTextToModel = () => {
+        if (sentence.trim() !== "") {
+            fetch(`http://localhost:8000/predict?sentence=${encodeURIComponent(sentence)}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setPrediction(data.emotion);
+                console.log(data.emotion);
+            })
+            .catch((error) => {
+                console.error("Error fetching prediction:", error);
+            });
+        } else {
+            setPrediction("");
+        }
+    };
+
+    useEffect(() => {
+        sendTextToModel();
+    }, [sentence]);
+
+    return (
+      <PredictionComponent sentence={sentence} prediction={prediction} setSentence={setSentence} />
   );
-}
+};
+
+export default App;
+
+
+
--- app-frontend/src/components/PredictionComponent.jsx
+++ app-frontend/src/components/PredictionComponent.jsx
@@ -2,30 +2,7 @@ import React, { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import '../css/PredictionComponent.css'
 
-const PredictionComponent = () => {
-    const [sentence, setSentence] = useState("");
-    const [prediction, setPrediction] = useState("");
-
-    const sendTextToModel = () => {
-        fetch(`http://localhost:8000/predict?sentence=${encodeURIComponent(sentence)}`)
-        .then((response) => response.json())
-        .then((data) => {
-            setPrediction(data.emotion);
-            console.log(data.emotion);
-        })
-        .catch((error) => {
-            console.error("Error fetching prediction:", error);
-        });
-    };
-
-    useEffect(() => {
-        if (sentence.trim() !== "") {
-            sendTextToModel();
-        } else {
-            setPrediction("");
-        }
-    }, [sentence]);    
-
+const PredictionComponent = ({ sentence, prediction, setSentence }) => {
     return (
         <div id="my-component">
             <h2>Enter a sentence below</h2>
